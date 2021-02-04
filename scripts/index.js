1ce495267eada3fc23d31472eea39d8d4adf64ac
Mike Zyatkov <mzyatkov@gmail.com>
2021-02-04 18:16:22 +0100

    feat: createElement

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const TODO_LIST = [
     { title: 'Полить цветы 🌸' },
     { title: 'Сделать свой сайт 👨‍💻' },
     { title: 'Погулять в парке 🌳' },
+    { title: '<img src="v" onerror="alert(\'Ваш сайт взломан\')" />' },
 ];
 
 const listContainerEl = document.querySelector('.todo__list');
@@ -11,10 +12,9 @@ const listContainerEl = document.querySelector('.todo__list');
 
 function render() {
     const html = TODO_LIST
-        .map(getItemHTML)
-        .join('')
+        .map(getItem)
 
-    listContainerEl.insertAdjacentHTML('afterbegin', html);
+    listContainerEl.append(...html);
 }
 
 
@@ -27,6 +27,33 @@ function getItemHTML(item) {
                         <button type="button" class="button button_remove"></button>
                     </div>
                 </li>`
+}
+
+
+function getItem(item) {
+    const cardItem = document.createElement('li');
+    cardItem.classList.add('todo__item', 'card');
+
+    const cardHeader = document.createElement('h2');
+    cardHeader.classList.add('card__title');
+    cardHeader.textContent = item.title;
+
+    const cardActions = document.createElement('div');
+    cardActions.classList.add('card__actions');
+
+    const editButton = document.createElement('button');
+    editButton.classList.add('button', 'button_edit');
+
+    const duplicateButton = document.createElement('button');
+    duplicateButton.classList.add('button', 'button_duplicate');
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('button', 'button_remove');
+
+    cardActions.append(editButton, duplicateButton, removeButton);
+    cardItem.append(cardHeader, cardActions);
+
+    return cardItem;
 }
 
 render();
