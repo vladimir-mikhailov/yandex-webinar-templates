bc881023b93b665f16ccfc27704c2692dfc62d77
Mike Zyatkov <mzyatkov@gmail.com>
2021-02-04 18:04:29 +0100

    feat: dynamic list rendering

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,28 @@ const TODO_LIST = [
     { title: 'Сделать свой сайт 👨‍💻' },
     { title: 'Погулять в парке 🌳' },
 ];
+
+const listContainerEl = document.querySelector('.todo__list');
+
+
+function render() {
+    const html = TODO_LIST
+        .map(getItemHTML)
+        .join('')
+
+    listContainerEl.insertAdjacentHTML('afterbegin', html);
+}
+
+
+function getItemHTML(item) {
+    return `<li class="todo__item card">
+                    <h2 class="card__title">${item.title}</h2>
+                    <div class="card__actions">
+                        <button type="button" class="button button_edit"></button>
+                        <button type="button" class="button button_duplicate"></button>
+                        <button type="button" class="button button_remove"></button>
+                    </div>
+                </li>`
+}
+
+render();
